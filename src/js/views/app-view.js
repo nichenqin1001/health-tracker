e9afcf8e403a7de2134afd6005387f8b23513d3a
nichenqin1001 <nichenqin@hotmail.com>
2017-04-11 15:58:06 +0800

    nf_calories

--- src/js/views/app-view.js
+++ src/js/views/app-view.js
@@ -12,7 +12,7 @@
             var searchFoodText = $('#seach-food-text').val();
             $.getJSON('https://api.nutritionix.com/v1_1/search/' + searchFoodText + '?', {
                 'results': '0:20',
-                'fields': 'item_name,brand_name,item_id,brand_id',
+                'fields': 'item_name,brand_name,item_id,brand_id,nf_calories',
                 'appId': 'e24d74f6',
                 'appKey': 'd9c92ac01b23ea5673b1de38ca46e84c'
             }, function (data) {
