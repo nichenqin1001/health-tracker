fa24c3d38b7bc209478943fc337b5d49ce1044af
nichenqin1001 <nichenqin@hotmail.com>
2017-07-04 14:07:43 +0800

    gh-pages init

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/index.css';
-
-import './js/app';
--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import AppView from './views/app-view';
-
-$(function () {
-
-    new AppView();
-
-});
